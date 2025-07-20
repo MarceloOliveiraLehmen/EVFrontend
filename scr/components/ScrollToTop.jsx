915d39e50aac6992c39cbb8f8a9164ba60a45350
MarceloOliveiraLehmen <marcelo.lehmen@hotmail.com>
2025-07-20 11:48:50 -0300

    Ajuste no ScrollToTop.jsx

--- scr/components/ScrollToTop.jsx
+++ scr/components/ScrollToTop.jsx
@@ -5,10 +5,14 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }, [pathname]);
 
   return null;
 }
 
 export default ScrollToTop;
+
